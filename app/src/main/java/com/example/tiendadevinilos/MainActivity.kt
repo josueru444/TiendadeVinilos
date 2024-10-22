41f7dbe868115572915934094e6bf2149bc9fbb8
pce <josueru444@gmail.com>
2024-10-22 08:44:54 -0600

    Creation of pages and navhost

--- app/src/main/java/com/example/tiendadevinilos/MainActivity.kt
+++ app/src/main/java/com/example/tiendadevinilos/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.tiendadevinilos.pages.HomePage
+import com.example.tiendadevinilos.pages.LoginPage
 import com.example.tiendadevinilos.ui.theme.TiendaDeVinilosTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,22 +18,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TiendaDeVinilosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = Routes.loginPage, builder = {
+                    composable(Routes.homePage) {
+                        HomePage(navController)
+                    }
+                    composable(Routes.loginPage) {
+                        LoginPage(navController)
+                    }
+                })
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
---
+++ app/src/main/java/com/example/tiendadevinilos/Routes.kt
@@ -0,0 +1,6 @@
+package com.example.tiendadevinilos
+
+object Routes {
+    var homePage = "homePage"
+    var loginPage = "loginPage"
+}
